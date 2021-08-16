4614aa7dedb2aad6770dbaacc2177bfac9475ee5
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2021-08-16 14:03:02 +0700

    intial commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,68 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="container">
+      <div class="row header mb-5 p-3">
+        <div class="col-lg-12">
+          <h2>What am I gonna do today</h2>
+        </div>
+      </div>
+      <div class="row content">
+        <div class="col-lg-12">
+          <div class="pekerjaan-rumah">
+            <div class="row text-start">
+              <div v-for="tugasSaya in tugas" v-bind:key="tugasSaya.id" class="tugas mb-3 col-lg-12">
+                <ItemTugas v-bind:tugasSaya="tugasSaya" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import "bootstrap/dist/css/bootstrap.css"
+
+import ItemTugas from './components/ItemTugas.vue'
 
 export default {
   name: 'App',
+  data: function() {
+    return {
+      tugas: [
+        {
+          id: 1,
+          namaTugas: "Menyapu Rumah",
+          isDone: false,
+        },
+        {
+          id: 2,
+          namaTugas: "Ngoding Android",
+          isDone: false,
+        },
+        {
+          id: 3,
+          namaTugas: "Beli SSD",
+          isDone: false,
+        },
+        {
+          id: 4,
+          namaTugas: "Makan Bakso",
+          isDone: false,
+        }
+      ]
+    }
+  },
   components: {
-    HelloWorld
+    ItemTugas
   }
 }
 </script>
 
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +70,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.header {
+  background: #1ABC9C;
+}
+
+.header h2 {
+  margin: 0;
+}
+
+.tugas .item {
+  font-size: 20px;
+}
+
+.item-completed {
+  color: #1ABC9C;
 }
 </style>
---
+++ src/components/ItemTugas.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div class="form-check">
+            <input class="form-check-input" onclick="" type="checkbox" value="" id="flexCheckDefault">
+            <label class="form-check-label item" v-bind:class="{'item-completed': tugasSaya.isDone}">
+                {{ tugasSaya.namaTugas }}
+            </label>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ItemTugas",
+    props: ['tugasSaya']
+}
+</script>
+
+<style scoped>
+
+</style>
